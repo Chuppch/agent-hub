8e0cd6bd24ef737adbdfcc1c3ae7087387c4342c
xiaoheng <732722834@qq.com>
2025-12-16 19:49:12 +0800

    fix: 调整AI Agent配置类

--- agent-hub-app/src/main/java/com/chuppch/config/AiAgentConfig.java
+++ agent-hub-app/src/main/java/com/chuppch/config/AiAgentConfig.java
@@ -6,6 +6,7 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,21 +21,16 @@ public class AiAgentConfig {
 
     // 初始化向量存储
     @Bean("vectorStore")
+    @ConditionalOnBean(name = "pgVectorJdbcTemplate")
     public PgVectorStore pgVectorStore(@Value("${spring.ai.openai.base-url}") String baseUrl,
                                        @Value("${spring.ai.openai.api-key}") String apiKey,
-                                       @Value("${spring.ai.openai.completions-path:v1/chat/completions}") String completionsPath,
-                                       @Value("${spring.ai.openai.embeddings-path:v1/embeddings}") String embeddingsPath,
                                        @Qualifier("pgVectorJdbcTemplate") JdbcTemplate jdbcTemplate) {
-        // 初始化 OpenAI API - 配置 OpenAI API 的 URL、API Key 和路径
-        // baseUrl + embeddingsPath = https://apis.itedus.cn/v1/embeddings
+
         OpenAiApi openAiApi = OpenAiApi.builder()
                 .baseUrl(baseUrl)
                 .apiKey(apiKey)
-                .completionsPath(completionsPath)
-                .embeddingsPath(embeddingsPath)
                 .build();
 
-        // 初始化 OpenAiEmbeddingModel - 实现数据的向量化
         OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
         return PgVectorStore.builder(jdbcTemplate, embeddingModel)
                 .vectorTableName("vector_store_openai")
