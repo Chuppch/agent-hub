aa455771585472305573f7f12a91a68fd830e68a
xiaoheng <732722834@qq.com>
2025-12-17 21:25:07 +0800

    feat: 完成对AiClientToolMcpNode节点的编写，实现对sse和stdio业务功能的实现

---
+++ agent-hub-domain/src/main/java/com/chuppch/domain/agent/service/armory/node/AiClientToolMcpNode.java
@@ -0,0 +1,131 @@
+package com.chuppch.domain.agent.service.armory.node;
+
+import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
+import com.alibaba.fastjson.JSON;
+import com.chuppch.domain.agent.model.entity.ArmoryCommandEntity;
+import com.chuppch.domain.agent.model.valobj.AiClientToolMcpVO;
+import com.chuppch.domain.agent.model.valobj.enums.AiAgentEnumVO;
+import com.chuppch.domain.agent.service.armory.node.factory.DefaultArmoryStrategyFactory;
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author chuppch
+ * @description
+ * @create 2025/12/17
+ */
+public class AiClientToolMcpNode extends AbstractArmorySupport {
+
+    @Resource
+    private AiClientModelNode aiClientModelNode;
+
+    @Override
+    protected String doApply(ArmoryCommandEntity requestParameter, DefaultArmoryStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("Ai Agent 构建节点，Tool MCP 工具配置{}", JSON.toJSONString(requestParameter));
+
+        List<AiClientToolMcpVO> aiClientToolMcpList = dynamicContext.getValue(dataName());
+
+        if (aiClientToolMcpList == null || aiClientToolMcpList.isEmpty()) {
+            log.warn("没有需要被初始化的 ai client tool mcp");
+            return router(requestParameter, dynamicContext);
+        }
+
+        for (AiClientToolMcpVO mcpVO : aiClientToolMcpList) {
+            // 创建 MCP 服务
+            McpSyncClient mcpSyncClient = createMcpSyncClient(mcpVO);
+            // 注册 MCP 服务
+            registerBean(beanName(mcpVO.getMcpId()), McpSyncClient.class, mcpSyncClient);
+        }
+
+        return router(requestParameter, dynamicContext);
+    }
+
+    @Override
+    public StrategyHandler<ArmoryCommandEntity, DefaultArmoryStrategyFactory.DynamicContext, String> get(ArmoryCommandEntity armoryCommandEntity, DefaultArmoryStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        return null;
+    }
+
+    // 根据MCP ID生成Bean名称
+    @Override
+    protected String beanName(String beanId) {
+        return AiAgentEnumVO.AI_CLIENT_TOOL_MCP.getBeanName(beanId);
+    }
+
+    // 获取数据名称
+    @Override
+    protected String dataName() {
+        return AiAgentEnumVO.AI_CLIENT_TOOL_MCP.getDataName();
+    }
+
+    
+    /*  
+        创建 MCP 同步客户端
+        @param aiClientToolMcpVO MCP 配置对象
+        @return MCP 同步客户端
+    */
+    private McpSyncClient createMcpSyncClient(AiClientToolMcpVO aiClientToolMcpVO) {
+        String transportType = aiClientToolMcpVO.getTransportType();
+
+        // 根据传输类型创建 MCP 客户端
+        switch (transportType) {
+            case "sse" -> {
+                AiClientToolMcpVO.TransportConfigSse transportConfigSse = aiClientToolMcpVO.getTransportConfigSse();
+                // http://127.0.0.1:9999/sse?apikey=DElk89iu8Ehhnbu
+                String originalBaseUri = transportConfigSse.getBaseUri();
+                String baseUri;
+                String sseEndpoint;
+
+                int queryParamStartIndex = originalBaseUri.indexOf("sse");
+                if (queryParamStartIndex != -1) {
+                    baseUri = originalBaseUri.substring(0, queryParamStartIndex - 1);
+                    sseEndpoint = originalBaseUri.substring(queryParamStartIndex - 1);
+                } else {
+                    baseUri = originalBaseUri;
+                    sseEndpoint = transportConfigSse.getSseEndpoint();
+                }
+
+                sseEndpoint = StringUtils.isBlank(sseEndpoint) ? "/sse" : sseEndpoint;
+
+                HttpClientSseClientTransport sseClientTransport = HttpClientSseClientTransport
+                        .builder(baseUri) // 使用截取后的 baseUri
+                        .sseEndpoint(sseEndpoint) // 使用截取或默认的 sseEndpoint
+                        .build();
+
+                McpSyncClient mcpSyncClient = McpClient.sync(sseClientTransport).requestTimeout(Duration.ofMinutes(aiClientToolMcpVO.getRequestTimeout())).build();
+                var init_sse = mcpSyncClient.initialize();
+
+                log.info("Tool SSE MCP Initialized {}", init_sse);
+                return mcpSyncClient;
+            }
+            case "stdio" -> {
+                AiClientToolMcpVO.TransportConfigStdio transportConfigStdio = aiClientToolMcpVO.getTransportConfigStdio();
+                Map<String, AiClientToolMcpVO.TransportConfigStdio.Stdio> stdioMap = transportConfigStdio.getStdio();
+                AiClientToolMcpVO.TransportConfigStdio.Stdio stdio = stdioMap.get(aiClientToolMcpVO.getMcpName());
+
+                // https://github.com/modelcontextprotocol/servers/tree/main/src/filesystem
+                var stdioParams = ServerParameters.builder(stdio.getCommand())
+                        .args(stdio.getArgs())
+                        .env(stdio.getEnv())
+                        .build();
+
+                var mcpClient = McpClient.sync(new StdioClientTransport(stdioParams))
+                        .requestTimeout(Duration.ofSeconds(aiClientToolMcpVO.getRequestTimeout())).build();
+                var init_stdio = mcpClient.initialize();
+
+                log.info("Tool Stdio MCP Initialized {}", init_stdio);
+                return mcpClient;
+            }
+        }
+        throw new RuntimeException("err! transportType " + transportType + " not exist!");
+    }
+
+}
