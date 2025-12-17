f1b54666374459f14a4d3f4a2a4e41703b0d2d62
xiaoheng <732722834@qq.com>
2025-12-17 15:56:49 +0800

    feat: 实现对责任链节点的初始化配置

---
+++ agent-hub-domain/src/main/java/com/chuppch/domain/agent/service/armory/node/AbstractArmorySupport.java
@@ -0,0 +1,85 @@
+package com.chuppch.domain.agent.service.armory.node;
+
+import cn.bugstack.wrench.design.framework.tree.AbstractMultiThreadStrategyRouter;
+import com.chuppch.domain.agent.model.entity.ArmoryCommandEntity;
+import com.chuppch.domain.agent.service.armory.node.factory.DefaultArmoryStrategyFactory;
+
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author chuppch
+ * @description
+ * @create 2025/12/17
+ */
+public abstract class AbstractArmorySupport extends AbstractMultiThreadStrategyRouter<ArmoryCommandEntity, DefaultArmoryStrategyFactory.DynamicContext, String> {
+
+    // 自定义日志配置器 - 允许自定义配置
+    private final Logger log = LoggerFactory.getLogger(AbstractArmorySupport.class);
+
+    // Spring上下文
+    @Resource
+    protected ApplicationContext applicationContext;
+
+    // 线程池
+    @Resource
+    protected ThreadPoolExecutor threadPoolExecutor;
+
+    // 仓储 - todo 完善
+    //@Resource
+    //protected IAgentRepository repository;
+
+    @Override
+    protected void multiThread(ArmoryCommandEntity requestParameter, DefaultArmoryStrategyFactory.DynamicContext dynamicContext) throws ExecutionException, InterruptedException, TimeoutException {
+        // 缺省的
+    }
+
+    protected String beanName(String id) {
+        return null;
+    }
+
+    protected String dataName() {
+        return null;
+    }
+
+    /**
+     * 通用的Bean注册方法 - 实例化的核心
+     *
+     * @param beanName  Bean名称
+     * @param beanClass Bean类型
+     * @param <T>       Bean类型
+     */
+    protected synchronized <T> void registerBean(String beanName, Class<T> beanClass, T beanInstance) {
+        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
+
+        // 注册Bean
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(beanClass, () -> beanInstance);
+        BeanDefinition beanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
+        beanDefinition.setScope(BeanDefinition.SCOPE_SINGLETON);
+
+        // 如果Bean已存在，先移除
+        if (beanFactory.containsBeanDefinition(beanName)) {
+            beanFactory.removeBeanDefinition(beanName);
+        }
+
+        // 注册新的Bean
+        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+
+        log.info("成功注册Bean: {}", beanName);
+    }
+
+    protected <T> T getBean(String beanName) {
+        return (T) applicationContext.getBean(beanName);
+    }
+
+
+}
