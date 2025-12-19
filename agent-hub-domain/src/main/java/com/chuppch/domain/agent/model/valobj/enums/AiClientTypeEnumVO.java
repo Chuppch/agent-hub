b023682deba8332acfe387cda89e4afb0278384c
xiaoheng <732722834@qq.com>
2025-12-19 22:27:02 +0800

    feat: 定义 AIClient 枚举类型，用户区分不同阶段使用的客户端配置

---
+++ agent-hub-domain/src/main/java/com/chuppch/domain/agent/model/valobj/enums/AiClientTypeEnumVO.java
@@ -0,0 +1,33 @@
+package com.chuppch.domain.agent.model.valobj.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author chuppch
+ * @description 定义 AIClient 枚举类型，用户区分不同阶段使用的客户端配置
+ * @create 2025/12/19
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public enum AiClientTypeEnumVO {
+
+    DEFAULT("DEFAULT", "通用的"),
+    TASK_ANALYZER_CLIENT("TASK_ANALYZER_CLIENT", "任务分析和状态判断"),
+    PRECISION_EXECUTOR_CLIENT("PRECISION_EXECUTOR_CLIENT", "具体任务执行"),
+    QUALITY_SUPERVISOR_CLIENT("QUALITY_SUPERVISOR_CLIENT", "质量检查和优化"),
+    RESPONSE_ASSISTANT("RESPONSE_ASSISTANT", "智能响应助手"),
+
+    TOOL_MCP_CLIENT("TOOL_MCP_CLIENT", "工具分析"),
+    PLANNING_CLIENT("PLANNING_CLIENT","任务规划"),
+    EXECUTOR_CLIENT("EXECUTOR_CLIENT", "任务执行")
+
+    ;
+
+    private String code;
+    private String info;
+
+}
